93c8227aa5025f63d4d7fe85e9ae7d1d1e921cde
김용선 <korea5781@gmail.com>
2020-05-29 19:41:15 +0900

    200529_07

--- BookStore/src/main/java/com/mycompany/domain/BookVO.java
+++ BookStore/src/main/java/com/mycompany/domain/BookVO.java
@@ -1,79 +1,104 @@
 package com.mycompany.domain;
 
-public class BookVO {   
-   private int  bookId;
-   private int  writerId;
-   private int  bookPrice;
-   private String  bookName;
-   private String  bookGenre;
-   private String  bookStory;
-   private String  bookPdate;
-   private int  bookSaleprice;
-   private int  bookCnt;
-   private int  bookScore;
-   
-   
-   public int getBookId() {
-      return bookId;
-   }
-   public void setBookId(int bookId) {
-      this.bookId = bookId;
-   }
-   public int getWriterId() {
-      return writerId;
-   }
-   public void setWriterId(int writerId) {
-      this.writerId = writerId;
-   }
-   public int getBookPrice() {
-      return bookPrice;
-   }
-   public void setBookPrice(int bookPrice) {
-      this.bookPrice = bookPrice;
-   }
-   public String getBookName() {
-      return bookName;
-   }
-   public void setBookName(String bookName) {
-      this.bookName = bookName;
-   }
-   public String getBookGenre() {
-      return bookGenre;
-   }
-   public void setBookGenre(String bookGenre) {
-      this.bookGenre = bookGenre;
-   }
-   public String getBookStory() {
-      return bookStory;
-   }
-   public void setBookStory(String bookStory) {
-      this.bookStory = bookStory;
-   }
-   public String getBookPdate() {
-      return bookPdate;
-   }
-   public void setBookPdate(String bookPdate) {
-      this.bookPdate = bookPdate;
-   }
-   public int getBookSaleprice() {
-      return bookSaleprice;
-   }
-   public void setBookSaleprice(int bookSaleprice) {
-      this.bookSaleprice = bookSaleprice;
-   }
-   public int getBookCnt() {
-      return bookCnt;
-   }
-   public void setBookCnt(int bookCnt) {
-      this.bookCnt = bookCnt;
-   }
-   public int getBookScore() {
-      return bookScore;
-   }
-   public void setBookScore(int bookScore) {
-      this.bookScore = bookScore;
-   }
-   
-   
+public class BookVO {
+	private int bookId;
+	private int writerId;
+	private int bookPrice;
+	private String bookName;
+	private String bookGenre;
+	private String bookStory;
+	private String bookPdate;
+	private int bookSaleprice;
+	private int bookCnt;
+	private int bookScore;
+	private String writerName;
+
+	public String getWriterName() {
+		return writerName;
+	}
+
+	public void setWriterName(String writerName) {
+		this.writerName = writerName;
+	}
+
+	public int getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+	public int getWriterId() {
+		return writerId;
+	}
+
+	public void setWriterId(int writerId) {
+		this.writerId = writerId;
+	}
+
+	public int getBookPrice() {
+		return bookPrice;
+	}
+
+	public void setBookPrice(int bookPrice) {
+		this.bookPrice = bookPrice;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public String getBookGenre() {
+		return bookGenre;
+	}
+
+	public void setBookGenre(String bookGenre) {
+		this.bookGenre = bookGenre;
+	}
+
+	public String getBookStory() {
+		return bookStory;
+	}
+
+	public void setBookStory(String bookStory) {
+		this.bookStory = bookStory;
+	}
+
+	public String getBookPdate() {
+		return bookPdate;
+	}
+
+	public void setBookPdate(String bookPdate) {
+		this.bookPdate = bookPdate;
+	}
+
+	public int getBookSaleprice() {
+		return bookSaleprice;
+	}
+
+	public void setBookSaleprice(int bookSaleprice) {
+		this.bookSaleprice = bookSaleprice;
+	}
+
+	public int getBookCnt() {
+		return bookCnt;
+	}
+
+	public void setBookCnt(int bookCnt) {
+		this.bookCnt = bookCnt;
+	}
+
+	public int getBookScore() {
+		return bookScore;
+	}
+
+	public void setBookScore(int bookScore) {
+		this.bookScore = bookScore;
+	}
 
 }
