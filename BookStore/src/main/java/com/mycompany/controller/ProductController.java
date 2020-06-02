187f1d56bef2f21042ba8fe9e89ac21675b9103a
김용선 <korea5781@gmail.com>
2020-06-02 09:15:51 +0900

    200602_01

--- BookStore/src/main/java/com/mycompany/controller/ProductController.java
+++ BookStore/src/main/java/com/mycompany/controller/ProductController.java
@@ -32,28 +32,28 @@ public class ProductController {
 
 	@RequestMapping("/productView.do")
 	public ModelAndView product(BookVO vo, HttpSession session) {
-		//제품번호 세팅
+		// 제품번호 세팅
 		ModelAndView mv = new ModelAndView();
 		BookVO book = bookService.selectBook(vo);
 		mv.addObject("priceBeforeDiscount", book.getBookSaleprice() + 3000);
 		mv.addObject("info", book);
-		
-		//로그인 상태라면
-		if(session.getAttribute("customer")!= null) {
-			CustomerVO customer = (CustomerVO)session.getAttribute("customer");
+
+		// 로그인 상태라면
+		if (session.getAttribute("customer") != null) {
+			CustomerVO customer = (CustomerVO) session.getAttribute("customer");
 			Map<String, String> tendencyMap = new HashMap<String, String>();
 			tendencyMap.put("customerId", customer.getCustomerId());
 			tendencyMap.put("genre", book.getBookGenre());
 			tendencyService.increaseTendency(tendencyMap);
 		}
-		
-		
+
 		mv.setViewName("/productView");
 		return mv;
 	}
-	//검색 후 리스트 구성
+
+	// 검색 후 리스트 구성
 	@RequestMapping("/productList.do")
-	public ModelAndView bookList(@RequestParam(value="searchWord") String searchWord) {
+	public ModelAndView bookList(@RequestParam(value = "searchWord") String searchWord) {
 		ModelAndView mv = new ModelAndView();
 		Map<String, String> search = new HashMap<String, String>();
 		search.put("searchWord", searchWord);
@@ -62,49 +62,50 @@ public class ProductController {
 		mv.setViewName("/productList");
 		return mv;
 	}
-	//검색 창 리스트 구성
-	@RequestMapping(value="/searchList.do", produces="application/json; charset=utf-8")
+
+	// 검색 창 리스트 구성
+	@RequestMapping(value = "/searchList.do", produces = "application/json; charset=utf-8")
 	@ResponseBody
-	public Map searchList(@RequestParam(value="searchWord") String searchWord){
-		//검색어가 없다면 null값을 반환 -> 아래줄을 처리하지 않으면 모든 리스트를 다 가져오게됨
-		if(searchWord == null || searchWord.equals(""))
+	public Map searchList(@RequestParam(value = "searchWord") String searchWord) {
+		// 검색어가 없다면 null값을 반환 -> 아래줄을 처리하지 않으면 모든 리스트를 다 가져오게됨
+		if (searchWord == null || searchWord.equals(""))
 			return null;
 		Map<String, String> search = new HashMap<String, String>();
 		search.put("searchWord", searchWord);
 		List<BookVO> searchList = bookService.searchListBook(search);
 		Map<String, Object> searchResult = new HashMap<String, Object>();
-		searchResult.put("checkAjax", "checkAjax11");
 		searchResult.put("searchResult", searchList);
 		return searchResult;
 	}
-	   @RequestMapping("/addCartList.do")
-	   public ModelAndView addCartList(BuyCartListVO vo, HttpSession session) {
-		   ModelAndView mv = new ModelAndView();
-		   CustomerVO logInState = (CustomerVO)session.getAttribute("customer");
-		   // 로그인 여부 판단
-		   if(logInState==null) {
-			   mv.setViewName("/login"); 
-		   }else {
-			   vo.setCustomerId(logInState.getCustomerId());
-			   // 장바구니 클릭 시 기존에 장바구니에 있는 상품인지 확인
-			   BuyCartListVO result = buyCartListService.checkDuplicateCartList(vo);
-			   if(result==null) {
-				   // 장바구니에 새로 추가
-				   buyCartListService.addCartList(vo);
-			   }else {
-				   // 장바구니 안 기존 상품의 개수 변경
-				   vo.setBuycartlistId(result.getBuycartlistId());
-				   buyCartListService.cartListChangeCnt(vo);
-			   }
-			   // cartList를 가져옴
-			   List<BuyCartListVO> list = buyCartListService.getCartList(vo);
-			   for(int i=0; i<list.size(); i++) {
-				   int bookTotalPrice = list.get(i).getBuycartlistCnt()*list.get(i).getBookSaleprice();
-				   list.get(i).setBookTotalPrice(bookTotalPrice);
-			   }
-			   mv.addObject("cartList", list);
-			   mv.setViewName("/shopping-cart");
-		   }
-		   return mv;
-	   } 
+	// 제품 메뉴창에서 장바구니를 갱신한 후에 모델로 넘김
+	@RequestMapping("/addCartList.do")
+	public ModelAndView addCartList(BuyCartListVO vo, HttpSession session) {
+		ModelAndView mv = new ModelAndView();
+		CustomerVO logInState = (CustomerVO) session.getAttribute("customer");
+		// 로그인 여부 판단
+		if (logInState == null) {
+			mv.setViewName("/login");
+		} else {
+			vo.setCustomerId(logInState.getCustomerId());
+			// 장바구니 클릭 시 기존에 장바구니에 있는 상품인지 확인
+			BuyCartListVO result = buyCartListService.checkDuplicateCartList(vo);
+			if (result == null) {
+				// 장바구니에 새로 추가
+				buyCartListService.addCartList(vo);
+			} else {
+				// 장바구니 안 기존 상품의 개수 변경
+				vo.setBuycartlistId(result.getBuycartlistId());
+				buyCartListService.cartListChangeCnt(vo);
+			}
+			// cartList를 가져옴
+			List<BuyCartListVO> list = buyCartListService.getCartList(vo);
+			for (int i = 0; i < list.size(); i++) {
+				int bookTotalPrice = list.get(i).getBuycartlistCnt() * list.get(i).getBookSaleprice();
+				list.get(i).setBookTotalPrice(bookTotalPrice);
+			}
+			mv.addObject("cartList", list);
+			mv.setViewName("/shopping-cart");
+		}
+		return mv;
+	}
 }
--- BookStore/src/main/webapp/resources/custom/js/custom_header.js
+++ BookStore/src/main/webapp/resources/custom/js/custom_header.js
@@ -1,12 +1,8 @@
 $(function(){
-	console.log("resources/custom/js/custom_header.js js파일 연결 확인");
-	
 	$('#listSearch').on('keyup', listSearchKeyUpEvent);
-	
 });
 
 function listSearchKeyUpEvent() {
-	console.log("resources/custom/js/custom_header.js js파일에서 listSearch keyUP 확인 ");
 	$.ajax({
 		type:'post', // get을 하나 post를 하나 url에 보이진 않음, 용량이 많으면 post
 		async:true, // default : true
@@ -15,7 +11,6 @@ function listSearchKeyUpEvent() {
 		data : {"searchWord" : $('#listSearch').val()},
 		dataType : 'json',
 		success : function(resultData){
-//			var list = resultData.searchResult;
 			var list = new Array();
 			list = resultData.searchResult;
 			makeSearhResultBox(list); // 검색 결과 컴포넌트를 구현
@@ -23,15 +18,11 @@ function listSearchKeyUpEvent() {
 	   error:function(request,status,error){
 		   console.log("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
 	   }
-
 	});
 }
 //검색 결과 컴포넌트를 구현하는 함수
 function makeSearhResultBox(list){
 	$('#searchList').empty(); //resultBox?를 초기화하고 새로 구성
-//	console.log(list[0].bookName);
-	console.log("custom_header.js makeSearchResultBox에서 확인  : " + list[0].bookName);
-	console.log("custom_header.js makeSearchResultBox에서 확인  : " + list[0].bookId);
 	var listPrefix = '<li class="list-group-item">';
 	var listSuffix = '</li>';
 	var spanPrefix = '<span>';
@@ -51,6 +42,4 @@ function makeSearhResultBox(list){
 		
 		$('#searchList').append(listContent);
 	}
-	
-	
 }
