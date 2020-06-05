1c212834355e8802e4f02e474a3ebb4aba9275fd
김용선 <korea5781@gmail.com>
2020-06-05 10:28:37 +0900

    200605_02 coverpage 수정

--- BookStore/src/main/webapp/resources/custom/js/scriptCover.js
+++ BookStore/src/main/webapp/resources/custom/js/scriptCover.js
@@ -1,37 +1,16 @@
-/**
- * 
- */
-	var selectFilter = $('.filter-control ul .active');
-	var filters = $('.filter-control ul li');
-	filters.click(function(){
-		selectFilter.removeClass('active');
-		selectFilter = $(this);
-		selectFilter.addClass("active");
+$(function(){
+	//회원가입 페이지 연결
+	$('#right-btn1').click(function(){
+		$(location).attr("href","moveToRegister.do");
 	});
-
-$(".product-slider").owlCarousel({
-    loop: true,
-    margin: 25,
-    nav: true,
-    items: 5,
-    dots: true,
-    navText: ['<i class="ti-angle-left"></i>', '<i class="ti-angle-right"></i>'],
-    smartSpeed: 1200,
-    autoHeight: false,
-    autoplay: true,
-    responsive: {
-        0: {
-            items: 1,
-        },
-        576: {
-            items: 2,
-        },
-        992: {
-            items: 4,
-        },
-        1200: {
-            items: 4,
-        }
-        
-    }
+	
+	//로그인 페이지 연결
+	$('#right-btn2').click(function(){
+		$(location).attr("href","moveToLogin.do");
+	});
+	
+	//메인 페이지 연결(**안함)
+	$('#right-btn3').click(function(){
+		$(location).attr("href","main.do");
+	});
 });
