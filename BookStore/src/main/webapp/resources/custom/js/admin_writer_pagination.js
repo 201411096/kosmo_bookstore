06d662e08d7729f07036307c0caac89e533accda
201411096 <korea5781@gmail.com>
2020-06-14 19:33:51 +0900

    200614_06 js 파일 설명 추가

--- BookStore/src/main/webapp/resources/custom/js/admin_writer_pagination.js
+++ BookStore/src/main/webapp/resources/custom/js/admin_writer_pagination.js
@@ -1,3 +1,25 @@
+/*
+변수
+	ㄴ curPage : 현재 페이지
+	ㄴ defaultOpts : 페이징 처리에 사용되는 기본 옵션
+		ㄴ 페이지를 클릭할 떄마다 해당 페이지에 맞는 데이터를 다시 불러오도록 이벤트를 걸어둠
+	
+함수
+	ㄴ getWriterDataInPaging : 페이지를 클릭했을 경우 불려오는 함수로 해당 페이지에 맞는 데이터를 불러옴
+	ㄴ getWriterData : 검색어를 포함하는 데이터들을 불러옴
+		ㄴ 내부적으로 동적인 페이징을 구성함
+	ㄴ drawWriterTable : ajax로 받아온 데이터로 테이블을 그려주는 함수
+	ㄴ updateBtnEvent : 수정 버튼들 눌렀을 경우 이벤트 핸들러
+		ㄴ 해당 bookId에 맞는 데이터들을 수정 화면으로 끌어가서 로딩함
+	ㄴ deleteBtnEvent : 삭제 버튼들 눌렀을 경우 이벤트 핸들러
+	
+실행부분
+	ㄴ 화면 로딩 후 바로 데이터를 가져와서 테이블를 그림
+	ㄴ 검색창에 입력할때마다 테이블을 다시 그려주는 keyup이벤트 핸들러 연결
+		ㄴ 동적 페이징 처리
+	ㄴ 수정 버튼 이벤트 핸들러 연결
+	ㄴ 삭제 버튼 이벤트 핸들러 연결
+*/
 var curPage;
 var writerData_total_page;
 var defaultOpts = {
